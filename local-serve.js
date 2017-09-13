99950fd07b555c539f06b30d9458b688c45ba31b
guillaumervls <guillaumervls@users.noreply.github.com>
2017-09-13 18:38:50 +0200

    Update local-serve.js

--- local-serve.js
+++ local-serve.js
@@ -7,8 +7,20 @@ import proxy from 'http-proxy-middleware';
 import schema from './schema';
 
 const app = express();
-app.use('/api/graphql', json(), graphqlExpress({ schema }));
-app.use('/api', graphiqlExpress({ endpointURL: '/api/graphql' }));
+
+if (process.env.API_ENDPOINT) {
+  app.use(
+    proxy('/api', {
+      target: process.env.API_ENDPOINT,
+      logLevel: 'debug',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    }),
+  );
+} else {
+  app.use('/api/graphql', json(), graphqlExpress({ schema }));
+  app.use('/api', graphiqlExpress({ endpointURL: '/api/graphql' }));
+}
 app.use(proxy({ target: 'http://localhost:3000', ws: true }));
 
 app.listen(8080, () => console.log('Server running on http://localhost:8080/'));
